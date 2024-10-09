f07b9d93a2ae222d3ba5c76de17339fa718a2d81
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-10-09 10:53:40 +0530

    Update

---
+++ Arrays/DecimalConversion.c
@@ -0,0 +1,46 @@
+// WIP
+// Program to convert Decimal numbers to any base
+
+#include <stdio.h>
+
+    int Conversion(int num, int base) {
+        int result[32];
+        int i = 0, j;
+        
+        while (num > 0) {
+            result[i] = num % base;
+            num = num / base;
+            i++;
+        }
+
+        if(base == 2){
+            printf("Binary: ");
+        }
+        else if(base == 8){
+            printf("Octal: ");
+
+        }
+        else if(base == 16){
+            printf("Hexadecimal: ");
+        }
+        else{
+            printf("Result: ");
+        }
+        for (j = i - 1; j >= 0; j--)
+            printf("%d", result[j]);
+        printf("\n");
+    }
+
+
+    int main() {
+        int num, k;
+        printf("Enter the decimal number: ");
+        scanf("%d", &num);
+
+        printf("Which base do you want to convert the decimal number into? ");
+        scanf("%d", &k);
+
+        Conversion(num, k);
+
+    return 0;
+}
---
+++ Arrays/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+
+            result[i] = num % k;
