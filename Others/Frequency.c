4cef1d710d7f2976b17f171bba53392babf93e4e
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-10-10 16:35:58 +0530

    Update

---
+++ Others/Frequency.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main(){
+    int i,j,arr[7]={0};
+
+    srand(time(0));
+
+    for (i=0;i<600;i++){
+        j=rand()%6;
+        j=j+1;
+        arr[j]++;
+    }
+
+    for(i=1;i<=6;i++)
+        printf("%d came out for %d times\n",i,arr[i]);
+
+    return 0;
+}
