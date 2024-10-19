309c00e29767635ff36403f0c1f75dd5e58de516
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-10-19 12:11:32 +0530

    Update

---
+++ Loops/Patterns/ForLoopPattern7.c
@@ -0,0 +1,36 @@
+/*
+* 
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i < 2 * n; i++){
+        if(i > n){
+            for(j = 2 * n - i; j > 0; j--){
+                printf("* ");
+            }
+        }
+        else{
+            for(j = 1; j <= i; j++){
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
