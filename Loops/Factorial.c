0649a4244e0ecf7a8926536be980bd19351c07de
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-10-23 13:01:36 +0530

    Minor update

--- Loops/Factorial.c
+++ Loops/Factorial.c
@@ -5,7 +5,7 @@ int main(){
     printf("Enter the number for which the factorial is required: ");
     scanf("%d", &n);
 
-    for(i=n; i>0; i--){
+    for(i = n; i > 0; i--){
         result *= i; 
     } 
     printf("The factorial of %d is %d.", n, result);
