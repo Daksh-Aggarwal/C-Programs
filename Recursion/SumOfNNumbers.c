30bcba68e55ee133c42a74df9336bb361c9480e7
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-11-05 10:08:48 +0530

    Added structures

---
+++ Recursion/SumOfNNumbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int summation(int);
+
+int main(){
+
+    int n, sum = 0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    summation(n);
+
+    return 0;
+}
+
+int summation(int n){
+    
+    if i > 0 return 
+}
