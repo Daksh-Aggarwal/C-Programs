9ec9bb1c77201874dd38ba9cd0490bc4e762fb5c
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-11-10 20:11:43 +0530

    Update

--- Recursion/FibonacciSeries.c
+++ Recursion/FibonacciSeries.c
@@ -1,10 +1,13 @@
-// WIP
-
 #include <stdio.h>
 
-int FibonacciSeries(int n, int arr[]){
-    
-
+int FibonacciSeries(int n){
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+    return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
 }
 
 int main(){
@@ -13,13 +16,9 @@ int main(){
     printf("How many elements of the series should be printed? ");
     scanf("%d", &n);
 
-    int arr[n];
-
-    FibonacciSeries(n, arr);
-
     printf("Here's the requested series:\n");
     for(int i = 0; i < n; i++){
-        printf("%d ", arr[i]);
+        printf("%d ", FibonacciSeries(i));
     }
 
     return 0;
