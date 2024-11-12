97340f289ff33b2c39479d870faec5923dd98605
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-11-12 12:58:12 +0530

    Update

---
+++ Functions/ArmstrongPerfectNumbers.c
@@ -0,0 +1,27 @@
+// WIP
+
+#include <stdio.h>
+#include <math.h>
+
+int isArmstrongNumber(int n){
+    int digits, digit, temp;
+
+    while(n > )
+
+}
+
+int isPerfectNumber(int n){
+
+}
+
+int main(){
+
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", n);
+
+    isArmstrongNumber(n);
+    isPerfectNumber(n);
+
+    return 0;
+}
