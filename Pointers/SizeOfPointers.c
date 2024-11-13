78716bc9a62cffe4cec4fe4d2f6d32c976428832
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-11-13 16:54:53 +0530

    Update

---
+++ Pointers/SizeOfPointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int *a;
+    float *b;
+    char *c;
+
+    printf("The size of int pointer variable is %d.\n", sizeof(a));
+    printf("The size of float pointer variable is %d.\n", sizeof(b));
+    printf("The size of char pointer variable is %d.", sizeof(c));
+
+    return 0;
+}
