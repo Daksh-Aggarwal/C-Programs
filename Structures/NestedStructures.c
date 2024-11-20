c6bee365092c2fe58f1370ea0d6ccfb08a923930
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-11-20 23:39:13 +0530

    Update

---
+++ Structures/NestedStructures.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct studentInfo {
+    char name[64];
+    int rollNumber;
+    float marks;
+};
+
+struct sections {
+    int section;
+    struct studentInfo *studentInfo;
+};
+
+int main() {
+    int sections, students;
+
+    printf("Enter number of sections: ");
+    scanf("%d", &sections);
+
+    printf("Enter number of students in each section: ");
+    scanf("%d", &students);
+
+    struct sections section[sections];
+
+    // Allocate memory for students in each section
+    for (int i = 0; i < sections; i++) {
+        section[i].section = i + 1;
+        section[i].studentInfo = malloc(students * sizeof(struct studentInfo));
+        }
+
+    // Input student details
+    for (int i = 0; i < sections; i++) {
+        printf("Enter details for section %d:\n", section[i].section);
+        for (int j = 0; j < students; j++) {
+            printf("Name: ");
+            scanf("%s", section[i].studentInfo[j].name);
+            printf("Roll number: ");
+            scanf("%d", &section[i].studentInfo[j].rollNumber);
+            printf("Marks: ");
+            scanf("%f", &section[i].studentInfo[j].marks);
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    // Print student details
+    for (int i = 0; i < sections; i++) {
+        printf("Details for section %d:\n", section[i].section);
+        for (int j = 0; j < students; j++) {
+            printf("Name: %s\n", section[i].studentInfo[j].name);
+            printf("Roll number: %d\n", section[i].studentInfo[j].rollNumber);
+            printf("Marks: %.2f\n", section[i].studentInfo[j].marks);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < sections; i++) {
+        free(section[i].studentInfo);
+    }
+
+    return 0;
+}
---
+++ Structures/StructureCopy.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+
+struct initial{
+    char name[64];
+    int id;
+    float marks;
+};
+
+// Complete copy
+struct copy_1{
+    char name[64];
+    int id;
+    float marks;
+};
+
+// Partial copy
+struct copy_2{
+    char name[64];
+    int id;
+    float marks;
+};
+
+int main(){
+    
+    int n;
+
+    printf("How many times do you want to input the data? ");
+    scanf("%d", &n);
+
+    struct initial initial[n];
+    struct copy_1 copy_1[n];
+    struct copy_2 copy_2[n];
+
+    for(int i = 0; i < n; i++){
+        printf("Enter the name for user %d: ", i + 1);
+        scanf("%s", &initial[i].name);
+        printf("Enter the ID for user %d: ", i + 1);
+        scanf("%d", &initial[i].id);
+        printf("Enter the marks for user %d: ", i + 1);
+        scanf("%f", &initial[i].marks);
+        printf("\n\n");
+    }
+
+    for(int i = 0; i < n; i++){
+        strcpy(copy_1[i].name, initial[i].name);
+        copy_1[i].id = initial[i].id;
+        copy_1[i].marks = initial[i].marks;
+        strcpy(copy_2[i].name, initial[i].name);
+        copy_2[i].marks = initial[i].marks;
+    }
+
+    printf("Original structure:\n");
+    for(int i = 0; i < n; i++){
+        printf("Details for student %d:\n", i + 1);
+        printf("Name: %s\n", initial[i].name);
+        printf("ID: %d\n", initial[i].id);
+        printf("Marks: %f\n", initial[i].marks);
+        printf("\n\n");
+    }
+
+    printf("Completely copied structure:\n");
+    for(int i = 0; i < n; i++){
+        printf("Details for student %d:\n", i + 1);
+        printf("Name: %s\n", copy_1[i].name);
+        printf("ID: %d\n", copy_1[i].id);
+        printf("Marks: %f\n", copy_1[i].marks);
+        printf("\n\n");
+    }
+
+    printf("Partially copied structure:\n");
+    for(int i = 0; i < n; i++){
+    printf("Details for student %d:\n", i + 1);
+    printf("Name: %s\n", copy_2[i].name);
+    printf("ID: %d\n", copy_2[i].id);
+    printf("Marks: %f\n", copy_2[i].marks);
+    printf("\n\n");
+    }
+
+    return 0;
+}
