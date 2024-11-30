4af1bb3b084e16a4915795085a1a51c4a01a82fb
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-11-30 19:51:48 +0530

    Update

--- File Handling/CountingCharsTabsSpacesLines.c
+++ File Handling/CountingCharsTabsSpacesLines.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int chars, tabs, spaces, lines;
+    int chars = 0, tabs = 0, spaces = 0, lines = 0;
     char ch;
     FILE *fp;
-    fp = fopen("pr1.c", "r");
+    fp = fopen("story.txt", "r");
 
     while(1){
         ch = fgetc(fp);
@@ -16,11 +16,10 @@ int main(){
         if(ch == '\t') tabs++;
         if(ch == ' ') spaces++;
         if(ch == '\n') lines++;
-
+    }
         fclose(fp);
 
         printf("No of. characters = %d\nNo. of tabs = %d\nNo. of spaces = %d\nNo. of lines = %d\n", chars, tabs, spaces, lines);
 
         return 0;
-    }
 }
