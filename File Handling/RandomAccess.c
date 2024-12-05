b375141ef59bdb76f9751b50f9da13c16de72b67
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-12-05 18:43:34 +0530

    Update

---
+++ File Handling/RandomAccess.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *file;
+
+    file = fopen("story.txt", "w+");
+
+    // Moving the write pointer to the 7th byte
+    fseek(file, 7, SEEK_SET);
+    fprintf(file, "C");
+
+    // Moving the  read pointer to the beginning
+    rewind(file);
+
+    // Reading and displaying the file content
+    char buffer[50];
+    fgets(buffer, sizeof(buffer), file);
+    printf("Updated file content: %s\n", buffer);
+
+    // Getting the current file pointer position
+    long pos = ftell(file);
+    printf("File pointer position after reading: %ld\n", pos);
+
+    fclose(file);
+    return 0;
+}
---
+++ Strings/Palindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char string[64];
+
+    printf("Enter the string: ");
+    scanf("%s", string);
+    
+    int length = strlen(string);
+
+    char revString[length];
+
+    for(int i = 0; i < length; i++){
+        revString[i] = string[length - i - 1];
+    }
+
+    int isAnagram = strcmp(string, revString);
+    if (isAnagram == 0) printf("The input string is an palindrome.");
+
+    return 0;
+}
