6d0fbf6696f3ac2dfeac806b18b5b55a63d74082
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2024-12-28 07:59:52 +0530

    Update

--- Others/GreatestNumber.c
+++ Others/GreatestNumber.c
@@ -5,17 +5,15 @@ int main(){
     printf("Enter the three numbers with a space between each (eg. 2 3 4): ");
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a>b && a>c){
+    if (a>=b && a>=c){
         printf("%d is the greatest number.", a);
     }
-    if (b>a && b>c){
+    else if (b>=a && b>=c){
         printf("%d is the greatest number.", b);
     }
-    if (c>a && c>b){
-        printf("%d is the greatest number.", c);
-    }
+    // else if (c>=a && c>=b){
     else{
-        printf("Error!");
+        printf("%d is the greatest number.", c);
     }
     
     return 0;
